360ccb2878fc36c70e49d8ddd820f3eee2036284
Rigan31 <mahedihasanrigan82@gmail.com>
2024-06-14 18:29:27 +0600

    Test file generator for task2

--- Task2/task2_test_case_generator.py
+++ Task2/task2_test_case_generator.py
@@ -4,19 +4,33 @@ import random
 def generate_transactions_and_queries(total_operation, start_timestamp):
     operations = []
     current_timestamp = 0
-    operations_type = []
+    
+    for _ in range(total_operation):
+        operation_type = ['Query', 'Transaction', 'Transaction', 'Transaction']
+        operation = random.choice(operation_type)
+
+        if operation == 'Query':
+            start_time = random.randint(0, start_timestamp)
+            end_time = start_time + random.randint(1, start_timestamp)
+            operations.append(('Query', start_time, end_time))
+        else:
+            current_timestamp += random.randint(1, 1000)
+            trans_type = random.choice(['Deposit', 'Withdraw'])
+            amount = random.randint(1, 10000)
+            operations.append((current_timestamp, trans_type, amount))
+    return operations
 
 
-def write_input_file(
-    input_folder_name, file_number, initial_reserve, transactions, queries
-):
+
+def write_input_file(input_folder_name, file_number, initial_reserve, operations):
     filename = input_folder_name + f"input_{file_number}.txt"
     with open(filename, "w") as f:
-        f.write(f"{initial_reserve} {len(transactions)} {len(queries)}\n")
-        for trans in transactions:
-            f.write(f"{trans[0]} {trans[1]} {trans[2]}\n")
-        for query in queries:
-            f.write(f"Query {query[0]} {query[1]}\n")
+        f.write(f"{initial_reserve}\n")
+        for operation in operations:
+            if len(operation) == 3:
+                f.write(f"{operation[0]} {operation[1]} {operation[2]}\n")
+            else:
+                f.write(f"Query {operation[1]} {operation[2]}\n")
 
 
 def main():
@@ -31,12 +45,8 @@ def main():
     input_folder_name = "input/"
 
     for i, (total_operation, start_timestamp) in enumerate(test_cases, start=1):
-        transactions, queries = generate_transactions_and_queries(
-            total_operation, start_timestamp
-        )
-        write_input_file(
-            input_folder_name, i, initial_reserve[i - 1], transactions, queries
-        )
+        operations= generate_transactions_and_queries(total_operation,start_timestamp)
+        write_input_file(input_folder_name, i, initial_reserve[i - 1],operations)
 
 
 if __name__ == "__main__":
